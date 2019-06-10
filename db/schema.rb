b0fcd655ec884db5dc462d39f3b02ca569f961f4
tatsuya <yokokura.tatsuya@gmail.com>
2019-06-10 17:10:33 +0900

    usersテーブルにimageカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_10_060309) do
+ActiveRecord::Schema.define(version: 2019_06_10_080143) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_06_10_060309) do
     t.string "facebook"
     t.string "twitter"
     t.string "homepage"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
