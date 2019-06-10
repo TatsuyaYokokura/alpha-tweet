029c99dd3bb89c70c31da5407e9c823759469ace
tatsuya <yokokura.tatsuya@gmail.com>
2019-06-10 15:18:53 +0900

    column追加 usersテーブル

---
+++ db/migrate/20190610060309_add_column_to_users.rb
@@ -0,0 +1,8 @@
+class AddColumnToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :nickname, :string, length: 8
+    add_column :users, :facebook, :string
+    add_column :users, :twitter, :string
+    add_column :users, :homepage, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_02_140105) do
+ActiveRecord::Schema.define(version: 2019_06_10_060309) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,10 @@ ActiveRecord::Schema.define(version: 2019_06_02_140105) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
+    t.string "facebook"
+    t.string "twitter"
+    t.string "homepage"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
