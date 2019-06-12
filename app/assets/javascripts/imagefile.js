90abb8eb5f1d07b8f9737d84feace925fcb39aff
tatsuya <yokokura.tatsuya@gmail.com>
2019-06-12 17:25:41 +0900

    ユーザー登録時に画像保存できるようにする

--- app/assets/javascripts/imagefile.js
+++ app/assets/javascripts/imagefile.js
@@ -2,6 +2,6 @@ $(function() {
   $('#user_avatar').on('change', function(e) {
       var file = e.target.files[0];
       var blobUrl = window.URL.createObjectURL(file);
-      $('#image').attr('src', blobUrl).css({'width':'200px', 'height':'150px', 'backbroud-size':'cover'});
+      $('#image').attr('src', blobUrl).css({'width':'200px', 'height':'150px', 'backgroud-size':'cover'});
   });
 });
