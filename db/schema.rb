446168d7c28c6b9f7f7c1cb8f328303e6e554ece
tatsuya <yokokura.tatsuya@gmail.com>
2019-06-13 17:12:35 +0900

    カラム削除とtweetsテーブルにレファレンス追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_10_082659) do
+ActiveRecord::Schema.define(version: 2019_06_12_083923) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -36,9 +36,10 @@ ActiveRecord::Schema.define(version: 2019_06_10_082659) do
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.string "description"
-    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -53,10 +54,10 @@ ActiveRecord::Schema.define(version: 2019_06_10_082659) do
     t.string "facebook"
     t.string "twitter"
     t.string "homepage"
-    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "tweets", "users"
 end
